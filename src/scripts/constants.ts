5a35ebac4b9137128c423a828e53fb69faf523e6
Michael Duren <michaeld@michaelduren.com>
2023-09-04 09:16:01 -0500

    animation sounds stop once user navigates away from home page

---
+++ src/scripts/constants.ts
@@ -0,0 +1,42 @@
+export const audioFiles = [
+  '/assets/keyboard-samples/1.wav',
+  '/assets/keyboard-samples/2.wav',
+  '/assets/keyboard-samples/3.wav',
+  '/assets/keyboard-samples/4.wav',
+  '/assets/keyboard-samples/5.wav',
+  '/assets/keyboard-samples/6.wav',
+  '/assets/keyboard-samples/7.wav',
+  '/assets/keyboard-samples/8.wav',
+  '/assets/keyboard-samples/9.wav',
+  '/assets/keyboard-samples/10.wav',
+];
+
+export const terminalCommands = [
+  {
+    command: 'Welcome',
+    output: [
+      'Welcome to my portfolio site, there are a lot of fun things to check out here 🎉',
+    ],
+  },
+  {
+    command: 'I am...',
+    output: [
+      "I'm Michael...",
+      'a',
+      'Software Engineer',
+      'Web Developer',
+      'Musician',
+      'Based in Twin Cities MN',
+    ],
+  },
+  {
+    command: 'Have Fun',
+    output: ['Have fun exploring my site!', '🥳🥳🥳'],
+  },
+];
+
+export const blip = '/assets/keyboard-samples/blip.wav';
+export const pluck = '/assets/keyboard-samples/pluck.mp3';
+
+export const caretUpBoldIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216.49 168.49a12 12 0 0 1-17 0L128 97l-71.51 71.49a12 12 0 0 1-17-17l80-80a12 12 0 0 1 17 0l80 80a12 12 0 0 1 0 17Z"/></svg>';
--- src/scripts/terminal.ts
+++ src/scripts/terminal.ts
@@ -1,26 +1,14 @@
+import { continueAnimation, isAnimationComplete } from '../store/store';
+import {
+  terminalCommands,
+  audioFiles,
+  blip,
+  pluck,
+  caretUpBoldIcon,
+} from './constants';
+
 // global vars
-
-let play = false;
 let sound = false;
-
-const audioFiles = [
-  '/assets/keyboard-samples/1.wav',
-  '/assets/keyboard-samples/2.wav',
-  '/assets/keyboard-samples/3.wav',
-  '/assets/keyboard-samples/4.wav',
-  '/assets/keyboard-samples/5.wav',
-  '/assets/keyboard-samples/6.wav',
-  '/assets/keyboard-samples/7.wav',
-  '/assets/keyboard-samples/8.wav',
-  '/assets/keyboard-samples/9.wav',
-  '/assets/keyboard-samples/10.wav',
-];
-
-const blip = '/assets/keyboard-samples/blip.wav';
-const pluck = '/assets/keyboard-samples/pluck.mp3';
-
-const caretUpBoldIcon =
-  '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216.49 168.49a12 12 0 0 1-17 0L128 97l-71.51 71.49a12 12 0 0 1-17-17l80-80a12 12 0 0 1 17 0l80 80a12 12 0 0 1 0 17Z"/></svg>';
 
 // functions
 
@@ -28,6 +16,10 @@ const sleep = (delay: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
+};
+
+const playSample = (sample: string) => {
+  new Audio(sample).play();
 };
 
 const typedTitle = async () => {
@@ -42,9 +34,6 @@ const typedTitle = async () => {
   await sleep(500);
 };
 
-const playSample = (sample: string) => {
-  new Audio(sample).play();
-};
 const playRandomAudio = (i?: number) => {
   if (i) {
     const audio = new Audio(audioFiles[i]);
@@ -56,19 +45,43 @@ const playRandomAudio = (i?: number) => {
   audio.play();
 };
 
-const pause = 400;
 const type = 200;
 const fast = 100;
 
 const longPause = 1500;
 
-const runTerminal = async (sound: boolean) => {
+const runTerminal = async (sound: boolean, completed: boolean) => {
   const terminalInput = document.querySelector('#terminal-input');
   const terminalOutput = document.querySelector('#terminal-output');
   if (!terminalInput || !terminalOutput) return;
 
+  if (completed) {
+    terminalInput.textContent = 'Have Fun';
+    for (let command of terminalCommands) {
+      for (let output of command.output) {
+        const li = document.createElement('li');
+        li.className = 'flex items-center gap-2 fade-in';
+
+        const iconDiv = document.createElement('div');
+        iconDiv.className = 'w-4 h-4 rotate-90';
+        iconDiv.innerHTML = caretUpBoldIcon;
+
+        const textDiv = document.createElement('div');
+        textDiv.textContent = output;
+
+        li.appendChild(iconDiv);
+        li.appendChild(textDiv);
+
+        terminalOutput.appendChild(li);
+        sleep(100);
+      }
+    }
+    return;
+  }
+
   for (let command of terminalCommands) {
     for (let i = 0; i < command.command.length; i++) {
+      if (!continueAnimation.get()) return;
       if (sound) playRandomAudio(i);
       terminalInput!.textContent += command.command[i];
       if (i % 4 === 0) {
@@ -78,6 +91,7 @@ const runTerminal = async (sound: boolean) => {
       }
     }
     for (let output of command.output) {
+      if (!continueAnimation.get()) return;
       if (sound) playSample(blip);
       const li = document.createElement('li');
       li.className = 'flex items-center gap-2 fade-in';
@@ -100,83 +114,44 @@ const runTerminal = async (sound: boolean) => {
       terminalInput.textContent = '';
     }
   }
+  isAnimationComplete.set(true);
 };
 
-const startAnimation = (sound: boolean) => {
-  play = true;
-  const terminalInput = document.querySelector('#terminal-input');
-  const terminalOutput = document.querySelector('#terminal-output');
-  if (!terminalInput || !terminalOutput) return;
-
-  terminalInput.textContent = '';
-  terminalOutput.innerHTML = '';
-
-  runTerminal(sound);
-};
-
+// functions to run on each page load by astro
 document.addEventListener('astro:page-load', () => {
   typedTitle();
-
   const navLinks = document.querySelectorAll('.nav-link');
   if (navLinks) {
     navLinks.forEach((link) => {
-      if (sound) {
-        link.addEventListener('click', () => {
+      link.addEventListener('click', () => {
+        if (sound) {
           playSample(pluck);
-        });
-      }
+        }
+        continueAnimation.set(false);
+      });
     });
   }
 
-  const modalButtonSound: HTMLButtonElement | null = document.querySelector(
-    '#modal-button-sound'
-  );
-  const modalButtonNoSound: HTMLButtonElement | null = document.querySelector(
-    '#modal-button-no-sound'
-  );
-  if (modalButtonSound && modalButtonNoSound) {
-    modalButtonSound.addEventListener('click', () => {
-      sound = true;
-      startAnimation(true);
-    });
-    modalButtonNoSound.addEventListener('click', () => {
-      sound = false;
-      startAnimation(false);
-    });
+  if (!isAnimationComplete.get()) {
+    // if animation has not run yet we want to allow the user to select sound or no sound
+    return;
   }
-
-  const homeButton = document.querySelector('#home-button');
-  if (homeButton) {
-    homeButton.addEventListener('click', () => {
-      startAnimation(sound);
-    });
-  }
+  runTerminal(sound, isAnimationComplete.get());
 });
 
-document.addEventListener('astro:route-change', () => {
-  play = false;
-});
-
-const terminalCommands = [
-  {
-    command: 'Welcome',
-    output: [
-      'Welcome to my portfolio site, there are a lot of fun things to check out here 🎉',
-    ],
-  },
-  {
-    command: 'I am...',
-    output: [
-      "I'm Michael...",
-      'a',
-      'Software Engineer',
-      'Web Developer',
-      'Musician',
-      'Based in Twin Cities MN',
-    ],
-  },
-  {
-    command: 'Have Fun',
-    output: ['Have fun exploring my site!', '🥳🥳🥳'],
-  },
-];
+const modalButtonSound: HTMLButtonElement | null = document.querySelector(
+  '#modal-button-sound'
+);
+const modalButtonNoSound: HTMLButtonElement | null = document.querySelector(
+  '#modal-button-no-sound'
+);
+if (modalButtonSound && modalButtonNoSound) {
+  modalButtonSound.addEventListener('click', () => {
+    sound = true;
+    runTerminal(true, false);
+  });
+  modalButtonNoSound.addEventListener('click', () => {
+    sound = false;
+    runTerminal(false, false);
+  });
+}
