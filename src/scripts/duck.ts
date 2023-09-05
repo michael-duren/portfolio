c845b5a703a6e6487c4921ee54162919b1995a12
Michael Duren <michaeld@michaelduren.com>
2023-09-05 16:24:18 -0500

    bug fix for duck sound

--- src/scripts/duck.ts
+++ src/scripts/duck.ts
@@ -1,28 +1,30 @@
 import { playSample, stopSample } from './audio.utils';
 import { sound } from '../store/store';
 
-const soundBool = sound.get() === 'true';
+document.addEventListener('astro:page-load', () => {
+  const soundBool = sound.get() === 'true';
 
-const squeekHigh: HTMLAudioElement | null =
-  document.querySelector('#squeek-high');
-const squeekLow: HTMLAudioElement | null =
-  document.querySelector('#squeek-low');
+  const squeekHigh: HTMLAudioElement | null =
+    document.querySelector('#squeek-high');
+  const squeekLow: HTMLAudioElement | null =
+    document.querySelector('#squeek-low');
 
-const rubberDuck = document.querySelector('#rubber-duck');
-if (rubberDuck) {
-  rubberDuck.addEventListener('click', () => {
-    if (squeekHigh && soundBool) {
-      stopSample(squeekHigh);
-      playSample(squeekHigh);
-    }
-  });
-}
-const rubberDuckBlue = document.querySelector('#rubber-duck-blue');
-if (rubberDuckBlue) {
-  rubberDuckBlue.addEventListener('click', () => {
-    if (squeekLow && soundBool) {
-      stopSample(squeekLow);
-      playSample(squeekLow);
-    }
-  });
-}
+  const rubberDuck = document.querySelector('#rubber-duck');
+  if (rubberDuck) {
+    rubberDuck.addEventListener('click', () => {
+      if (squeekHigh && soundBool) {
+        stopSample(squeekHigh);
+        playSample(squeekHigh);
+      }
+    });
+  }
+  const rubberDuckBlue = document.querySelector('#rubber-duck-blue');
+  if (rubberDuckBlue) {
+    rubberDuckBlue.addEventListener('click', () => {
+      if (squeekLow && soundBool) {
+        stopSample(squeekLow);
+        playSample(squeekLow);
+      }
+    });
+  }
+});
