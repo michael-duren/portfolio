69979dbdfbda8d3645732c660f26955865ef9a49
Michael Duren <michaeld@michaelduren.com>
2023-09-05 16:14:45 -0500

    Fix bug with rendering finished animation

--- src/scripts/nav-audio.ts
+++ src/scripts/nav-audio.ts
@@ -1,4 +1,4 @@
-import { continueAnimation } from '../store/store';
+import { continueAnimation, isAnimationComplete } from '../store/store';
 import { sound } from '../store/store';
 
 const addNavLinkListeners = () => {
@@ -10,6 +10,7 @@ const addNavLinkListeners = () => {
         const pluck = new Audio('/audio/pluck.mp3');
         if (soundBool) pluck.play();
         continueAnimation.set(false);
+        isAnimationComplete.set(true);
       });
     });
   }
