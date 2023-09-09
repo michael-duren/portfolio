6498fe061e0bd6cee1c6468a055aa0cb52e1630f
Michael Duren <michaeld@michaelduren.com>
2023-09-09 15:50:33 -0500

    edit work area for mobile views

--- src/types/portfolio-item.ts
+++ src/types/portfolio-item.ts
@@ -1,7 +1,8 @@
 export type PortfolioItem = {
   title: string;
-  description: string;
+  description: string[];
   websiteLink: string;
   codeLink: string;
   images: ImageMetadata[];
+  color: string;
 };
