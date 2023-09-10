01fd98b8084fb17e1c9712653783f4bf93a33b37
Michael Duren <michaeld@michaelduren.com>
2023-09-09 20:48:27 -0500

    setup email for contact form

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,15 @@
-import { persistentAtom } from "@nanostores/persistent";
-import { atom } from "nanostores";
-import { logger } from "@nanostores/logger";
+import { persistentAtom } from '@nanostores/persistent';
+import { atom } from 'nanostores';
+import { logger } from '@nanostores/logger';
 
 const isDev = import.meta.env.DEV;
 
 export const isAnimationComplete = atom(false);
 export const continueAnimation = atom(true);
 export const startAnimation = atom(false);
-type Sound = "true" | "false";
-export const sound = persistentAtom<Sound>("sound", "true");
+type Sound = 'true' | 'false';
+export const sound = persistentAtom<Sound>('sound', 'true');
+export const formSuccessful = atom(false);
 
 if (isDev) {
   logger({
